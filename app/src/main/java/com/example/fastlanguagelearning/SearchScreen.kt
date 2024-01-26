f3603f7521078bc277999cf286169575c9d7a27d
LeonardoWAP <leonardowillersap@gmail.com>
2024-01-26 15:34:43 -0300

    Add retrofit in project

--- app/src/main/java/com/example/fastlanguagelearning/SearchScreen.kt
+++ app/src/main/java/com/example/fastlanguagelearning/SearchScreen.kt
@@ -3,15 +3,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.example.fastlanguagelearning.api.Endpoint
+import com.example.fastlanguagelearning.util.NetworkUtils
+import com.google.gson.JsonArray
 import kotlinx.serialization.json.Json
 import models.SearchResponse
 import org.json.JSONArray
-import java.net.HttpURLConnection
-import java.net.URL
+import retrofit2.Call
+import retrofit2.Response
 
 class SearchScreen : AppCompatActivity() {
 
@@ -19,64 +23,76 @@ class SearchScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val editText = findViewById<EditText>(R.id.input_word)
+        val insertedWordEditText = findViewById<EditText>(R.id.input_word)
         val searchButton =  findViewById<Button>(R.id.search_button)
+
+        searchButton.setOnClickListener {
+            val wordToSearch = insertedWordEditText.text.toString()
+            Thread{
+                getWordMeaning(wordToSearch)
+            }.start()
+        }
+
+        insertedWordEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(insertedWord: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(insertedWord: CharSequence?, start: Int, before: Int, count: Int) {
+                showSearchButton(insertedWord, searchButton)
+            }
+
+            override fun afterTextChanged(insertedWord: Editable?) {
+                setInsertedWordBold(insertedWord, insertedWordEditText)
+            }
+        })
+    }
+
+    fun setInsertedWordBold(insertedWord: CharSequence?, editText: EditText){
+        if (insertedWord.isNullOrEmpty()) {
+            editText.setTypeface(null, android.graphics.Typeface.NORMAL)
+        } else {
+            editText.setTypeface(null, android.graphics.Typeface.BOLD)
+        }
+    }
+
+    fun showSearchButton(insertedWord: CharSequence?, searchButton: Button){
+        if (insertedWord.isNullOrEmpty()) {
+            searchButton.visibility =  View.INVISIBLE
+        } else {
+            searchButton.visibility =  View.VISIBLE
+        }
+    }
+    private fun getWordMeaning(word: String){
+        val retrofitClient = NetworkUtils.getRetrofitInstance("https://api.dictionaryapi.dev/")
+        val endpoint = retrofitClient.create(Endpoint::class.java)
 
         val json = Json {
             ignoreUnknownKeys = true
         }
 
-        searchButton.setOnClickListener {
-            val wordToSearch = editText.text.toString()
-            Thread{
-                val url = URL("https://api.dictionaryapi.dev/api/v2/entries/en/${wordToSearch}")
-                val conn = url.openConnection() as HttpURLConnection
+        endpoint.getWordMeaning(word).enqueue(object : retrofit2.Callback<JsonArray>{
+            override fun onResponse(call: Call<JsonArray>, response: Response<JsonArray>) {
 
-                try{
-                    val data = conn.inputStream.bufferedReader().readText()
+               val jsonArray = JSONArray(response.body().toString())
 
-                   val jsonArray = JSONArray(data)
-                   val jsonStringList = mutableListOf<String>()
-                    for (i in 0 until jsonArray.length()) {
-                    jsonStringList.add(jsonArray.getJSONObject(i).toString())
-                   }
+               val jsonStringList = mutableListOf<String>()
+               for (i in 0 until jsonArray.length()) {
+                   jsonStringList.add(jsonArray.getJSONObject(i).toString())
+               }
+                val searchResponseList = jsonStringList.map {json.decodeFromString<SearchResponse>(it)}
 
-                   val searchResponseList = jsonStringList.map {
-                       json.decodeFromString<SearchResponse>(it)
-                   }
-                    switchToResultScreen(searchResponseList[0])
-                }finally {
-                    conn.disconnect()
-                }
-            }.start()
-        }
-
-        editText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                switchToResultScreen(searchResponseList[0])
             }
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (s.isNullOrEmpty()) {
-                    searchButton.visibility =  View.INVISIBLE
-                } else {
-                    searchButton.visibility =  View.VISIBLE
-                }
+            override fun onFailure(call: Call<JsonArray>, t: Throwable) {
+                Log.d("Failure to search word", word)
             }
 
-            override fun afterTextChanged(s: Editable?) {
-                if (s.isNullOrEmpty()) {
-                    editText.setTypeface(null, android.graphics.Typeface.NORMAL)
-                } else {
-                    editText.setTypeface(null, android.graphics.Typeface.BOLD)
-                }
-            }
         })
     }
 
     private fun switchToResultScreen(searchResponse: SearchResponse) {
         val intent = Intent(this, SearchResultScreen::class.java)
         intent.putExtra("searchResponse", searchResponse)
-
         startActivity(intent)
     }
 
