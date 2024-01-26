e363cb8947703cae10e1dd3f08da90fc318420ec
LeonardoWAP <leonardowillersap@gmail.com>
2024-01-26 16:14:19 -0300

    Config Cache

--- app/src/main/java/com/example/fastlanguagelearning/SearchResultScreen.kt
+++ app/src/main/java/com/example/fastlanguagelearning/SearchResultScreen.kt
@@ -42,8 +42,8 @@ class SearchResultScreen : AppCompatActivity() {
         }
 
         searchButton.setOnClickListener {
-            //switchToSearchScreen()
-            switchToPurchaseScreen()
+            switchToSearchScreen()
+           // switchToPurchaseScreen()
         }
     }
 
